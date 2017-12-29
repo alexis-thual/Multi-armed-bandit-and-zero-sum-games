2b868470732aed0bb5ac6f535d09c686f509b951
alexisthual <alexisthual@gmail.com>
2017-12-29 15:37:41 +0100

    Add player class

--- normalFormGame.py
+++ normalFormGame.py
@@ -3,7 +3,10 @@ import numpy as np
 
 # %% NFG class
 class NormalFormGame:
-    def __init__(self, verbose=True):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+        self.nActions = [2,2]
         self.matrix = np.array([
             [[4,3], [-1,-1]],
             [[0,0], [3,4]]
@@ -11,9 +14,22 @@ class NormalFormGame:
 
     def rewards(self, actions):
         '''
-        Input:
-            actions as a k-uplet (a1, ..., ak)
-        Output:
-            rewards for each players
+        Input: actions as a 2-uplet (a1, a2)
+        Output: rewards for each players
         '''
         return self.matrix[actions]
+
+    def bestReward(self, playerIndex, opponentAction):
+        # If column player:
+        if playerIndex == 0:
+            return np.max(self.matrix[opponentAction, :, playerIndex]))
+        else:
+            return np.max(self.matrix[:, opponentAction, playerIndex]))
+
+# %% Test cell
+matrix = np.array([
+    [[4,3], [-1,-1]],
+    [[0,0], [3,4]]
+])
+
+matrix[0,:,0]
---
+++ player.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+# %% Player cell
+class Player:
+    def __init__(self, verbose=False):
+        self.verbose = verbose
